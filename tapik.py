d4e6d9eaae5ecdd82ccffc353ab4948078e808b0
TheZakMan <TheZakMan@ctf-br.org>
2024-01-02 17:29:49 -0300

    improved the code

--- tapik.py
+++ tapik.py
@@ -2,6 +2,7 @@
 
 import requests
 import argparse
+import json
 
 def banner():
     banner = """
@@ -18,9 +19,9 @@ def banner():
     print(banner)
 
 def process_response(response, verbose):
-    error_messages = ["PERMISSION_DENIED", "INVALID_ARGUMENT", "REQUEST_DENIED"]
+    error_messages = ["PERMISSION_DENIED", "INVALID_ARGUMENT", "REQUEST_DENIED", "REJECTED", "BLOCKED"]
     for error_message in error_messages:
-        if error_message in response.text:
+        if error_message in response.text.upper():
             return error_message  # Return the specific error message
     return response.text if verbose else "WORKED"
 
@@ -83,8 +84,51 @@ def test_google_fonts_api(api_key, verbose):
     response = requests.get(url)
     return process_response(response, verbose)
 
-def test_api_keys(api_keys, verbose):
+def test_google_street_view_static_api(api_key, verbose):
+    url = f"https://maps.googleapis.com/maps/api/streetview?size=400x400&location=46.414382,10.013988&key={api_key}"
+    response = requests.get(url)
+    return process_response(response, verbose)
+
+def test_google_cloud_storage_api(api_key, verbose):
+    url = f"https://www.googleapis.com/storage/v1/b/myproject/iam?key={api_key}" #myproject
+    response = requests.get(url)
+    return process_response(response, verbose)
+
+def test_google_drive_api(api_key, verbose):
+    url = f"https://www.googleapis.com/drive/v3/files?key={api_key}"
+    response = requests.get(url)
+    return process_response(response, verbose)
+
+def test_google_sheets_api(api_key, verbose):
+    url = f"https://sheets.googleapis.com/v4/spreadsheets/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/values/Class%20Data?key={api_key}"
+    response = requests.get(url)
+    return process_response(response, verbose)
+
+def test_google_vision_api(api_key, verbose):
+    url = "https://vision.googleapis.com/v1/images:annotate"
+    headers = {"X-Goog-Api-Key": api_key, "Content-Type": "application/json"}
+    data = {
+        "requests": [
+            {
+                "image": {
+                    "source": {
+                        "imageUri": "https://64.media.tumblr.com/fc25fe4f65bc3d59cd7d51c98978bbc1/018960ee17970ed1-29/s128x128u_c1/a52339ef99aa53482b6719e226b03f47a87cee31.pnj"  # URL da imagem
+                    }
+                },
+                "features": [
+                    {"type": "LABEL_DETECTION"}  # Tipo de análise, e.g., detecção de rótulos
+                ]
+            }
+        ]
+    }
+    response = requests.post(url, json=data, headers=headers)
+    return process_response(response, verbose)
+
+
+def test_api_keys(api_keys, verbose, output_file=None):
+    api_results = {}
     for key in api_keys:
+        api_results[key] = {}
         spacer = "─" * 60
         title = "Testing API Key:"
         top = len(title + key) + 3
@@ -94,7 +138,7 @@ def test_api_keys(api_keys, verbose):
         print(f"╰{top2}╯")
 
         def print_test_result(api_name, test_function):
-            error_messages = ["PERMISSION_DENIED", "INVALID_ARGUMENT", "REQUEST_DENIED"]
+            error_messages = ["PERMISSION_DENIED", "INVALID_ARGUMENT", "REQUEST_DENIED", "REJECTED", "BLOCKED"]
             result = test_function(key, verbose)
             
             if result in error_messages:
@@ -103,6 +147,7 @@ def test_api_keys(api_keys, verbose):
             else:
                 status = "✅ [WORKED]"
                 print(f"{status} | {api_name}")
+                api_results[key][api_name] = result
                 if verbose:
                     print(result)
             print(spacer)
@@ -118,6 +163,16 @@ def test_api_keys(api_keys, verbose):
         print_test_result("Google Civic Information API", test_google_civic_information_api)
         print_test_result("Google Blogger API", test_google_blogger_api)
         print_test_result("Google Fonts API", test_google_fonts_api)
+        print_test_result("Google Street View Static API", test_google_street_view_static_api)
+        print_test_result("Google Cloud Storage API", test_google_cloud_storage_api)
+        print_test_result("Google Drive API", test_google_drive_api)
+        print_test_result("Google Sheets API", test_google_sheets_api)
+        print_test_result("Google Vision API", test_google_vision_api)
+    
+    if output_file and api_results:
+        with open(output_file, 'w') as file:
+            json.dump(api_results, file, indent=4)
+        print(f"[!] Results saved to: {output_file}")
 
 def main():
     try:
@@ -125,24 +180,17 @@ def main():
         parser.add_argument('-k', '--key', help='Single API key to test')
         parser.add_argument('-l', '--list', help='File containing list of API keys, one per line')
         parser.add_argument('-v', '--verbose', action='store_true', help='Print full responses for successful tests')
+        parser.add_argument('-o', '--output', help='Output file to save the results')
 
         args = parser.parse_args()
 
         if args.key:
-            test_api_keys([args.key], args.verbose)
+            test_api_keys([args.key], args.verbose, args.output)
         elif args.list:
             with open(args.list, 'r') as file:
                 keys = file.read().splitlines()
-                print(f"[Total of keys]: {len(keys)}")
-               
-                test_api_keys(keys, args.verbose)
-
-                #Debugging output
-                '''
-                print("Keys read from file:")
-                for k in keys:
-                    print(f"'{k}'")
-                '''
+                print(f"[Total of keys]: {len(keys)}")               
+                test_api_keys(keys, args.verbose, args.output)
         else:
             print("No API key or list provided. Use -k to provide a single key or -l to provide a list of keys.")
     
